23d621ada244847ec746c0536130cef558ae1a1d
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-06-23 19:07:51 -0400

    Create simple cpu struct, with flag setters and getters

---
+++ src/cpu.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef enum flag {
+	FLAG_CARRY = 0,
+	FLAG_PARITY = 2,
+	FLAG_AUX_CARRY = 4,
+	FLAG_ZERO = 6,
+	FLAG_SIGN = 7
+} flag;
+
+typedef struct cpu {
+	uint8_t A;
+	uint8_t B;
+	uint8_t C;
+	uint8_t D;
+	uint8_t E;
+	uint8_t H;
+	uint8_t L;
+	uint8_t flags;
+	uint16_t SP;
+	uint16_t PC;
+} cpu;
+
+void set_flag(cpu * cpu, flag flag) {
+	cpu->flags |= (1 << (int)flag);
+}
+
+void clear_flag(cpu * cpu, flag flag) {
+	cpu->flags &= ~(1 << (int)flag);
+}
+
+int get_flag(cpu * cpu, flag flag) {
+	return (cpu->flags >> (int)flag) & 1;
+}
---
+++ src/main.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include "cpu.h"
+
+int main(void) {
+
+	// create a new cpu
+	cpu * cpu = (struct cpu *)malloc(sizeof(cpu));
+
+	return 0;
+}
