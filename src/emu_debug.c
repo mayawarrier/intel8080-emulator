d4a16bca9a02511d53bc3f1118ed50e061fa4043
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-07-01 23:58:30 -0400

    New memory dump format - Commas between bytes, and a new line every 8 words

--- src/emu_debug.c
+++ src/emu_debug.c
@@ -14,7 +14,11 @@ void dump_memory(const word_t * memory, addr_t highest_addr) {
     
     if (dumpf_ptr != NULL) {
         for (size_t i = 0; i <= (size_t)highest_addr; ++i) {
-            fprintf(dumpf_ptr, WORD_T_FORMAT "\t", memory[i]);
+            fprintf(dumpf_ptr, WORD_T_FORMAT ",", memory[i]);
+            // End-line every 8 bytes
+            if ((i + 1) % 8 == 0) {
+                fprintf(dumpf_ptr, "\n");
+            }
         }
     }
 }
