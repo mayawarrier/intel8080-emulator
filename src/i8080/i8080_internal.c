510cf1cec2ec49bc7db09e4871fbb06f1e21ce33
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-07-06 20:07:36 -0400

    Implement all ADD instructions - This implements instructions 0x80 to 0x87 - After each operation that modifies flags z,s,p, call update_ZSP() and update any other flags as required - BIT_PAST_WORD is a buffer with the bit just past the end of word_t set - BIT_PAST_HALF_WORD is a buffer with the bit just past half of word_t set - WORD_LO_F is a buffer with the lower half bits of word_t set - i8080_read_memory() and i8080_write_memory() perform read and write from memory with the address in HL

--- src/i8080/i8080_internal.c
+++ src/i8080/i8080_internal.c
@@ -5,80 +5,35 @@
 
 #include "i8080_internal.h"
 #include "i8080_opcodes.h"
+#include <math.h>
 
 struct register_pair {
     word_t * left;
     word_t * right;
 };
 
-// Returns pointers to the left and right registers for a mov operation.
-static struct register_pair i8080_mov_get_register_pair(i8080 * const cpu, word_t opcode) {
-    
-    struct register_pair regs;
-    regs.left = NULL;
-    regs.right = NULL;
-    
-    word_t lo_opcode = opcode & 0x0f;
-    word_t hi_opcode = opcode & 0xf0;
-    
-    switch(lo_opcode) {
-        case 0x00: case 0x08:
-            regs.right = &cpu->b; break;
-        case 0x01: case 0x09: 
-            regs.right = &cpu->c; break;
-        case 0x02: case 0x0a:
-            regs.right = &cpu->d; break;
-        case 0x03: case 0x0b:
-            regs.right = &cpu->e; break;
-        case 0x04: case 0x0c:
-            regs.right = &cpu->h; break;
-        case 0x05: case 0x0d:
-            regs.right = &cpu->l; break;
-        case 0x07: case 0x0f:
-            regs.right = &cpu->a; break;
-        // Moving from M = [HL] should be dealt with separately
-        default: regs.right = NULL; break;
-    }
-    
-    switch(hi_opcode) {
-        case 0x40:
-            if (lo_opcode >= 0x0 && lo_opcode <= 0x07) {
-                regs.left = &cpu->b;
-            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
-                regs.left = &cpu->c;
-            }
-            break;
-        case 0x50:
-            if (lo_opcode >= 0x0 && lo_opcode <= 0x07) {
-                regs.left = &cpu->d;
-            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
-                regs.left = &cpu->e;
-            }
-            break;
-        case 0x60:
-            if (lo_opcode >= 0x0 && lo_opcode <= 0x07) {
-                regs.left = &cpu->h;
-            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
-                regs.left = &cpu->l;
-            }
-        case 0x70:
-            // Moving into M = [HL] should be dealt with separately
-            if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
-                regs.left = &cpu->a;
-            }
-            break;
-        default: regs.left = NULL;
-    }
-    
-    return regs;
-}
+static const buf_t BIT_PAST_WORD = 1 << (HALF_WORD_SIZE * 2);
+static const buf_t BIT_PAST_HALF_WORD = 1 << (HALF_WORD_SIZE);
+static const buf_t WORD_LO_F = pow(2, HALF_WORD_SIZE) - 1;
 
-static void i8080_mov_registers(i8080 * const cpu, word_t opcode) {
-    // get the register pair from this opcode
-    struct register_pair regs = i8080_mov_get_register_pair(cpu, opcode);
-    // perform move
-    *regs.left = *regs.right;
-}
+// Picks the lower half of the word
+#define WORD_LO_BITS(expr) ((buf_t)expr & WORD_LO_F)
+
+// Returns pointers to the left and right registers for a mov operation.
+static struct register_pair mov_get_register_pair(i8080 * const cpu, word_t opcode);
+// Updates the Z, S, P flags based on accumulator value
+static inline void update_ZSP(i8080 * const cpu);
+
+// Performs a mov operation from register to register.
+static void i8080_mov_registers(i8080 * const cpu, word_t opcode);
+// Performs an add to accumulator and updates flags.
+static void i8080_add(i8080 * const cpu, word_t word);
+
+static inline word_t i8080_read_memory(i8080 * const cpu);
+static inline void i8080_write_memory(i8080 * const cpu, word_t word);
+
+// Get address represented by {HL}
+static inline addr_t i8080_get_hl(i8080 * const cpu);
 
 static word_t i8080_advance_read_word(i8080 * const cpu) {
     // Read a word and advance program counter
@@ -114,7 +69,7 @@ void i8080_exec(i8080 * const cpu, word_t opcode) {
         case MOV_B_M: case MOV_C_M: case MOV_D_M: case MOV_E_M: case MOV_H_M: case MOV_L_M: case MOV_A_M:
         {
             // Read a word at address [HL]
-            word_t mem_word = cpu->read_memory((addr_t)cpu->h << HALF_ADDR_SIZE | (addr_t)cpu->l);
+            word_t mem_word = i8080_read_memory(cpu);
             
             switch(opcode) {
                 case MOV_B_M: cpu->b = mem_word; break;
@@ -144,8 +99,121 @@ void i8080_exec(i8080 * const cpu, word_t opcode) {
             }
             
             // Write a word at address [HL]
-            cpu->write_memory((addr_t)cpu->h << HALF_ADDR_SIZE | (addr_t)cpu->l, word_to_write);
+            i8080_write_memory(cpu, word_to_write);
             break;
         }
+        
+        // Regular addition
+        case ADD_B: i8080_add(cpu, cpu->b); break;
+        case ADD_C: i8080_add(cpu, cpu->c); break;
+        case ADD_D: i8080_add(cpu, cpu->d); break;
+        case ADD_E: i8080_add(cpu, cpu->e); break;
+        case ADD_H: i8080_add(cpu, cpu->h); break;
+        case ADD_L: i8080_add(cpu, cpu->l); break;
+        case ADD_M: i8080_add(cpu, i8080_read_memory(cpu)); break;
+        case ADD_A: i8080_add(cpu, cpu->a); break;
     }
+}
+
+static inline word_t i8080_read_memory(i8080 * const cpu) {
+    return cpu->read_memory(i8080_get_hl(cpu));
+}
+
+static inline void i8080_write_memory(i8080 * const cpu, word_t word) {
+    cpu->write_memory(i8080_get_hl(cpu), word);
+}
+
+static inline addr_t i8080_get_hl(i8080 * const cpu) {
+    return (addr_t)cpu->h << HALF_ADDR_SIZE | (addr_t)cpu->l;
+}
+
+static struct register_pair mov_get_register_pair(i8080 * const cpu, word_t opcode) {
+    
+    struct register_pair regs;
+    regs.left = NULL;
+    regs.right = NULL;
+    
+    word_t lo_opcode = opcode & 0x0f;
+    word_t hi_opcode = opcode & 0xf0;
+    
+    switch(lo_opcode) {
+        case 0x00: case 0x08:
+            regs.right = &cpu->b; break;
+        case 0x01: case 0x09: 
+            regs.right = &cpu->c; break;
+        case 0x02: case 0x0a:
+            regs.right = &cpu->d; break;
+        case 0x03: case 0x0b:
+            regs.right = &cpu->e; break;
+        case 0x04: case 0x0c:
+            regs.right = &cpu->h; break;
+        case 0x05: case 0x0d:
+            regs.right = &cpu->l; break;
+        case 0x07: case 0x0f:
+            regs.right = &cpu->a; break;
+        // Moving from M = [HL] should be dealt with separately
+        default: regs.right = NULL; break;
+    }
+    
+    switch(hi_opcode) {
+        case 0x40:
+            if (lo_opcode >= 0x00 && lo_opcode <= 0x07) {
+                regs.left = &cpu->b;
+            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
+                regs.left = &cpu->c;
+            }
+            break;
+        case 0x50:
+            if (lo_opcode >= 0x00 && lo_opcode <= 0x07) {
+                regs.left = &cpu->d;
+            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
+                regs.left = &cpu->e;
+            }
+            break;
+        case 0x60:
+            if (lo_opcode >= 0x00 && lo_opcode <= 0x07) {
+                regs.left = &cpu->h;
+            } else if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
+                regs.left = &cpu->l;
+            }
+        case 0x70:
+            // Moving into M = [HL] should be dealt with separately
+            if (lo_opcode >= 0x08 && lo_opcode <= 0x0f) {
+                regs.left = &cpu->a;
+            }
+            break;
+        default: regs.left = NULL;
+    }
+    
+    return regs;
+}
+
+static void i8080_mov_registers(i8080 * const cpu, word_t opcode) {
+    // get the register pair from this opcode
+    struct register_pair regs = mov_get_register_pair(cpu, opcode);
+    // perform move
+    *regs.left = *regs.right;
+}
+
+static inline void update_ZSP(i8080 * const cpu) {
+    cpu->z = (cpu->a == 0);
+    cpu->s = (cpu->a < 0);
+    cpu->p = 0; buf_t buf = (buf_t)cpu->a; 
+    /* Until all 8 bits have been shifted out,
+     * shift each bit to the left and XNOR it with cpu->p.
+     * while(buf & (word_t)WORD_T_MAX != 0): check if word bits are zero yet
+     * (buff<<=1): shift the buffer to the left
+     * & BIT_PAST_WORD: select the bit we just shifted
+     * == 0: invert the bit before the XOR */
+    while(buf & (word_t)WORD_T_MAX != 0) { 
+        cpu->p ^= ((buf<<=1) & BIT_PAST_WORD == 0); 
+    }
+}
+
+static void i8080_add(i8080 * const cpu, word_t word) {
+    cpu->a += word;
+    // Update flags
+    update_ZSP(cpu);
+    cpu->acy = (WORD_LO_BITS(cpu->a) + WORD_LO_BITS(word)) & BIT_PAST_HALF_WORD != 0;
+    cpu->cy = ((buf_t)cpu->a + (buf_t)word) & BIT_PAST_WORD != 0;
 }
