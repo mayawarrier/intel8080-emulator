e9f4993ae785ba6a7710ed1b79a5079b2bf7d2c8
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-07-06 22:38:48 -0400

    Add dump_cpu_stats(), dumps values of all registers and flags

--- src/emu_debug.c
+++ src/emu_debug.c
@@ -5,8 +5,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "emu_debug.h"
+#include "i8080/i8080.h"
 
 static const char * DUMP_LOCATION = "memory_dump.bin";
+static const char * CPU_STATS_DUMP_LOCATION = "cpu_stats_dump.bin";
 
 void dump_memory(const word_t * memory, addr_t highest_addr) {
     
@@ -20,5 +22,28 @@ void dump_memory(const word_t * memory, addr_t highest_addr) {
                 fprintf(dumpf_ptr, "\n");
             }
         }
+    }
+}
+
+void dump_cpu_stats(i8080 * const cpu) {
+    
+    FILE * dumpf_ptr = fopen(CPU_STATS_DUMP_LOCATION, "w");
+    
+    if (dumpf_ptr != NULL) {
+        fprintf(dumpf_ptr, "A: " WORD_T_FORMAT "\n", cpu->a);
+        fprintf(dumpf_ptr, "B: " WORD_T_FORMAT "\n", cpu->b);
+        fprintf(dumpf_ptr, "C: " WORD_T_FORMAT "\n", cpu->c);
+        fprintf(dumpf_ptr, "D: " WORD_T_FORMAT "\n", cpu->d);
+        fprintf(dumpf_ptr, "E: " WORD_T_FORMAT "\n", cpu->e);
+        fprintf(dumpf_ptr, "H: " WORD_T_FORMAT "\n", cpu->h);
+        fprintf(dumpf_ptr, "L: " WORD_T_FORMAT "\n", cpu->l);
+        fprintf(dumpf_ptr, "PC: " ADDR_T_FORMAT "\n", cpu->pc);
+        fprintf(dumpf_ptr, "SP: " ADDR_T_FORMAT "\n", cpu->sp);
+        fprintf(dumpf_ptr, "Zero: %d\n", cpu->z);
+        fprintf(dumpf_ptr, "Sign: %d\n", cpu->s);
+        fprintf(dumpf_ptr, "Parity: %d\n", cpu->p);
+        fprintf(dumpf_ptr, "Auxiliary carry: %d\n", cpu->acy);
+        fprintf(dumpf_ptr, "Carry: %d\n", cpu->cy);
+        fprintf(dumpf_ptr, "Interrupt enable: %d\n", cpu->ie);
     }
 }
--- src/emu_debug.h
+++ src/emu_debug.h
@@ -11,8 +11,11 @@
 #define EMU_DEBUG_H
 
 #include "emu_types.h"
+#include "i8080/i8080.h"
 
 // Dumps the contents of memory to file
 void dump_memory(const word_t * memory, addr_t highest_addr);
+// Dumps the values of all registers and flags
+void dump_cpu_stats(i8080 * const cpu);
 
 #endif /* EMU_DEBUG_H */
