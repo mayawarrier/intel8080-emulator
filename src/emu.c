b1dfbebe09468f9c539056b160bf5930f4670b74
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-08-03 22:42:26 -0400

    WIP: Add a simple command processor at WBOOT (untested) - WBOOT now calls a command processor that supports two commands: RUN addr, and QUIT - RUN addr runs a program starting at addr - QUIT quits the emulator - Collapse all the memory_create_default_bootloader() etc into emu_set_default_env. -- This is because most programs will not require such fidelity. If they're not targeting CP/M then they'll have a custom environment which they can load in through memory_load() anyway.

--- src/emu.c
+++ src/emu.c
@@ -13,54 +13,24 @@
 static const emu_word_t DEFAULT_BOOTLOADER[] = {
     JMP,
     0x00,
-    DEFAULT_START_OF_PROGRAM_MEMORY
+    DEFAULT_START_PA
 };
 
 static const int DEFAULT_BOOTLOADER_SIZE = 3;
 
-bool memory_init(emu_mem_t * const memory_handle) {
+// The console port address duplicated across 16-bit address bus for use with port out
+static const emu_addr_t CONSOLE_ADDR_FULL = (emu_addr_t)((CPM_CONSOLE_ADDR << HALF_ADDR_SIZE) | CPM_CONSOLE_ADDR);
+
+bool memory_init(emu_word_t * const memory_buf) {
     // Allocate memory
-    memory_handle->mem = (emu_word_t *)malloc(sizeof(emu_word_t) * (ADDR_T_MAX + 1));
-    
-    if (memory_handle->mem == NULL) {
+    memory_buf = (emu_word_t *)malloc(sizeof(emu_word_t) * (ADDR_T_MAX + 1));
+    if (memory_buf == NULL) {
         printf("Error: Could not allocate enough memory to emulate.\n");
         return false;
     }
-    
     // Zero out the entire memory
-    memset((void *)memory_handle->mem, 0, ADDR_T_MAX + 1);
-    // set size
-    memory_handle->highest_addr = ADDR_T_MAX;
-    
+    memset((void *)memory_buf, 0, ADDR_T_MAX + 1);
     return true;
-}
-
-emu_addr_t memory_setup_IVT(emu_mem_t * const memory_handle) {
-    
-    // Create the interrupt vector table
-    for (int i = 0; i < NUM_IVT_LOCATIONS; ++i) {
-        // HLT for all interrupts
-        memory_handle->mem[INTERRUPT_TABLE[i]] = HLT;
-    }
-    
-    return DEFAULT_START_OF_PROGRAM_MEMORY;
-}
-
-bool memory_write_bootloader(emu_mem_t * const memory_handle, const emu_word_t * bootloader, size_t bootloader_size) {
-    
-    if (bootloader_size > 8) {
-        printf("Error: bootloader too large.\n");
-        return false;
-    }
-    
-    // Copy the bootloader into memory, this is executed on RESET
-    memcpy((void *)memory_handle->mem, (const void *)bootloader, bootloader_size);
-    
-    return true;
-}
-
-void memory_write_bootloader_default(emu_mem_t * const memory_handle) {
-    memory_write_bootloader(memory_handle, DEFAULT_BOOTLOADER, DEFAULT_BOOTLOADER_SIZE);
 }
 
 size_t memory_load(const char * file_loc, emu_word_t * memory, emu_addr_t start_loc) {
@@ -71,7 +41,7 @@ size_t memory_load(const char * file_loc, emu_word_t * memory, emu_addr_t start_
     
     if (f_ptr == NULL) {
         printf("Error: file cannot be opened.\n");
-        return SIZE_MAX;
+        goto failure;
     }
     
     // get the file size
@@ -82,7 +52,7 @@ size_t memory_load(const char * file_loc, emu_word_t * memory, emu_addr_t start_
     // check if it can fit into memory
     if (file_size + start_loc > ADDR_T_MAX + 1) {
         printf("Error: file too large.\n");
-        return SIZE_MAX;
+        goto failure;
     }
     
     // Attempt to read the entire file
@@ -90,17 +60,38 @@ size_t memory_load(const char * file_loc, emu_word_t * memory, emu_addr_t start_
     
     if (words_read != file_size) {
         printf("Error: file read failure.\n");
-        return SIZE_MAX;
+        goto failure;
     }
     
     printf("Success: %zu words read.\n\n", words_read);
     
     fclose(f_ptr);
     return words_read;
+    
+    failure: {
+        fclose(f_ptr);
+        return 0;
+    }
+}
+
+// Prints the string at str_addr to CPM console, terminated by '$'
+static void cpm_print_str(i8080 * const cpu, emu_addr_t str_addr) {
+    // Print each character until we hit a '$'
+    while (true) {
+        emu_word_t str_char = cpu->read_memory(str_addr);
+        if (str_char == '$') {
+            break;
+        } else {
+            // cpu->port_out can decide how to format the characters
+            cpu->port_out(CONSOLE_ADDR_FULL, str_char);
+            str_addr++;
+        }
+    }
 }
 
 // Provides limited emulation of CP/M BDOS and zero page. At the moment, this only works with BDOS op 2 and 9, and WBOOT.
-bool i8080_cpm_zero_page(void * const udata) {
+// WBOOT launches into a simple command processor so programs can be run and the emulator can quit.
+static bool i8080_cpm_zero_page(void * const udata) {
     
     i8080 * const cpu = (i8080 * const)udata;
 
@@ -109,10 +100,72 @@ bool i8080_cpm_zero_page(void * const udata) {
     
     switch(ret_addr) {
         
-        case 0x01: 
+        case 0xe401:
         {
-            // This is a warm boot to CP/M. Quit the emulator for now until supported
-            return false;
+            // This is a basic command processor, entered upon WBOOT. Commands are: RUN addr, and QUIT.
+            // RUN addr starts executing instructions from addr. addr should be in 16-bit hex format eg. 0x0000 or 0x1234
+            // QUIT quits the emulator.
+            
+            // Addresses of command proc messages
+            const emu_addr_t ERROR_ADDR_PTR = 0x40;                     // "\nInvalid address."
+            const emu_addr_t ERROR_CMD_PTR = ERROR_ADDR_PTR + 0x18;     // "\nInvalid command."
+            const emu_addr_t CMD_PROMPT_PTR = ERROR_CMD_PTR + 0x18;     // "> "
+            
+            // Max length, excluding trailing null
+            int LEN_INPUT_BUF = 127;
+            emu_word_t input_buf[LEN_INPUT_BUF + 1];
+            
+            // Address from RUN command
+            emu_addr_t run_addr;
+            
+            // Keep getting input from the console until a valid command is entered
+            while(true) {
+                // Prints a prompt "> "
+                cpm_print_str(cpu, CMD_PROMPT_PTR);
+                // reset buffer
+                int buf_loc = 0;
+                
+                // Get console input till end of line
+                while (buf_loc != LEN_INPUT_BUF) {
+                    emu_word_t ch = cpu->port_in(CONSOLE_ADDR_FULL);
+                    if (ch == '\n') break;
+                    input_buf[buf_loc++] = ch;
+                }
+                input_buf[buf_loc] = '\0';
+
+                // Process commands
+                if (strncmp(input_buf, "RUN ", 4) == 0) {
+                    // ADDR_T_FORMAT is %#06x, # formats for 0x in every case except 0x0000
+                    if (strncmp(&input_buf[4], "0x0000", 6) == 0) {
+                        run_addr = 0x0000;
+                    } else if (sscanf(&input_buf[4], ADDR_T_FORMAT, &run_addr) == 1
+                            && run_addr >= 0 && run_addr <= 0xffff) {
+                        // address is in correct format and within bounds
+                        goto command_run;
+                    } else {
+                        // Invalid address error
+                        cpm_print_str(cpu, ERROR_ADDR_PTR);
+                    }
+                } else if (strncmp(input_buf, "QUIT", 4) == 0) {
+                    goto command_quit;
+                } else {
+                    // Invalid command error
+                    cpm_print_str(cpu, ERROR_CMD_PTR);
+                }
+                // Print a newline
+                cpu->port_out(CONSOLE_ADDR_FULL, '\n');
+            }
+            
+            command_run: {
+                emu_word_t lo_addr = (emu_word_t)(run_addr & WORD_T_MAX);
+                emu_word_t hi_addr = (emu_word_t)((run_addr >> HALF_WORD_SIZE) & WORD_T_MAX);
+                cpu->write_memory(0xe401, JMP);
+                cpu->write_memory(0xe402, lo_addr);
+                cpu->write_memory(0xe403, hi_addr);
+                return true;
+            }
+            
+            command_quit: return false;
         }
         
         case 0x06: 
@@ -127,24 +180,14 @@ bool i8080_cpm_zero_page(void * const udata) {
                     // Writes an output string terminated by '$'
                     // Address of string is {DE}
                     emu_addr_t str_addr = (emu_addr_t)((cpu->d << HALF_ADDR_SIZE) | cpu->e);
-
-                    // Print each character until we hit a '$'
-                    while (true) {
-                        emu_word_t str_char = cpu->read_memory(str_addr);
-                        if (str_char == '$') {
-                            break;
-                        } else {
-                            printf(WORD_T_PRT_FORMAT, str_char);
-                            str_addr++;
-                        }
-                    }
+                    cpm_print_str(cpu, str_addr);
                     break;
                 }
 
                 case 2: 
                 {
                     // Writes the character in register E
-                    printf(WORD_T_PRT_FORMAT, cpu->e);
+                    cpu->port_out(CONSOLE_ADDR_FULL, cpu->e);
                     break;
                 }
             }
@@ -159,20 +202,62 @@ bool i8080_cpm_zero_page(void * const udata) {
 void emu_set_cpm_env(i8080 * const cpu) {
     // 0x38 is a special instruction that calls
     // an external fn outside the i8080 runtime.
-    // This is used to emulate a call to CP/M zero page fns.
+    // This is used to emulate calls to CP/M BDOS and WBOOT, and
+    // provide a basic command processor.
     
     if (cpu->write_memory != NULL) {
         // Entry for CP/M BDOS is 0x05.
-        cpu->write_memory(0x05, 0x38);
-        cpu->write_memory(0x06, 0xc9);
+        cpu->write_memory(0x05, EMU_EXT_CALL);
+        cpu->write_memory(0x06, RET);
 
-        // Entry to CP/M WBOOT (warm boot)
-        cpu->write_memory(0x00, 0x38);
-        cpu->write_memory(0x01, 0xc9);
+        // Entry to CP/M WBOOT (warm boot). Jumps to command processor,
+        // which lies at 0xe400 for 64K machines
+        cpu->write_memory(0x00, JMP);
+        cpu->write_memory(0x01, 0x00);
+        cpu->write_memory(0x02, 0xe4);
+        
+        // External call to a simple command processor
+        cpu->write_memory(0xe400, EMU_EXT_CALL);
+        
+        // Command processor messages, '$'-terminated
+        // as is the convention in CP/M.
+        char ** CMD_MSGS = {"\nInvalid address.$", "\nInvalid command.$", "> $"};
+        
+        // Store all messages after the standard IVT
+        emu_addr_t msgs_loc = 0x40;
+        for (int i = 0; i < 3; ++i) {
+            char * msg = CMD_MSGS[i];
+            for (int j = 0; j < strlen(msg); ++j) {
+                cpu->write_memory(msgs_loc++, msg[j]);
+            }
+        }
+        
+        // HLT for the rest of the interrupts
+        for (int i = 1; i < NUM_IVT_VECTORS; ++i) {
+            cpu->write_memory(INTERRUPT_TABLE[i], HLT);
+        }
 
         cpu->emu_ext_call = i8080_cpm_zero_page;
     } else {
         printf("i8080 streams have not been initialized yet!");
+    }
+}
+
+void emu_set_default_env(i8080 * const cpu) {
+    
+    // Create the interrupt vector table
+    // Do not write to RST 1 sequence, we'll put our bootloader there instead
+    for (int i = 1; i < NUM_IVT_VECTORS; ++i) {
+        // HLT for all interrupts
+        cpu->write_memory(INTERRUPT_TABLE[i], HLT);
+    }
+    
+    // Write a default bootloader that jumps to start of program memory
+    emu_addr_t wr_addr = INTERRUPT_TABLE[0]; // start at 0x0
+    
+    for (int i = 0; i < DEFAULT_BOOTLOADER_SIZE; ++i) {
+        cpu->write_memory(wr_addr, DEFAULT_BOOTLOADER[i]);
+        wr_addr++;
     }
 }
 
@@ -183,6 +268,8 @@ void emu_init_i8080(i8080 * const cpu) {
     cpu->port_in = NULL;
     cpu->port_out = NULL;
     cpu->emu_ext_call = NULL;
+    // interrupts disabled by default
+    cpu->ie = false;
 }
 
 bool emu_setup_streams(i8080 * const cpu, read_word_fp read_memory, write_word_fp write_memory, 
@@ -205,14 +292,14 @@ bool emu_setup_streams(i8080 * const cpu, read_word_fp read_memory, write_word_f
     return true;
 }
 
-bool emu_runtime(i8080 * const cpu, emu_mem_t * const memory) {
+bool emu_runtime(i8080 * const cpu) {
     
     cpu->cycles_taken = 0;
     
     cpu->pc = 0x100;
     
     // debug
-    dump_memory(memory->mem, memory->highest_addr);
+    //dump_memory(memory->mem, memory->highest_addr);
     
     // execute all the instructions until told to stop
     while(true) {
--- src/emu.h
+++ src/emu.h
@@ -15,22 +15,20 @@
 #include "emu_types.h"
 #include "i8080/i8080.h"
 
-#define DEFAULT_START_OF_PROGRAM_MEMORY 0x40
+/* The starting location of the CP/M Transient Program Area i.e. 
+ * the first valid location to load a program written for CP/M. 
+ * Set this environment with emu_set_cpm_env(). */
+static const emu_addr_t CPM_START_OF_TPA = 0x100;
+// Port address that CP/M will treat as its console
+static const emu_word_t CPM_CONSOLE_ADDR = 0x00;
+/* The first valid location to load a program with the default 
+ * environment. Set this environment with emu_set_default_env(). */
+static const emu_addr_t DEFAULT_START_PA = 0x40;
 
 // Allocates the largest amount of memory addressable (in this case 64KB)
-bool memory_init(emu_mem_t * const memory_handle);
+bool memory_init(emu_word_t * const memory_buf);
 // Loads a file into memory. Call only after memory_init(). Returns number of words read.
 size_t memory_load(const char * file_loc, emu_word_t * memory, emu_addr_t start_loc);
-
-// Create a default interrupt vector table at the top 64 bytes of memory.
-// Returns the address from which it is safe to load the program without overwriting the IVT.
-emu_addr_t memory_setup_IVT(emu_mem_t * const memory_handle);
-// Write a bootloader to the RESET/RST 0 interrupt sequence.
-// This must be no more than 8 bytes.
-// Returns false if the bootloader is too large.
-bool memory_write_bootloader(emu_mem_t * const memory_handle, const emu_word_t * bootloader, size_t bootloader_size);
-// Writes a bootloader that simply jumps to the DEFAULT_START_OF_PROGRAM_MEMORY.
-void memory_write_bootloader_default(emu_mem_t * const memory_handle);
 
 // Initialize an i8080
 void emu_init_i8080(i8080 * const cpu);
@@ -38,15 +36,20 @@ void emu_init_i8080(i8080 * const cpu);
 // I/O read/write are allowed to be NULL, but the emulator will crash if an I/O request is made.
 bool emu_setup_streams(i8080 * const cpu, read_word_fp read_memory, write_word_fp write_memory, 
         read_word_fp io_port_in, write_word_fp io_port_out);
-// Sets up the environment for some basic CP/M 2.2 operating system emulation.
-// Call this only after the streams have been set.
+
+/* Sets up the environment for some basic CP/M 2.2 BIOS emulation.
+ * Some programs written for the 8080 need this environment. At the moment, only 
+ * supports CP/M BDOS ops 2 and 9, and WBOOT (which simply quits the emulator).
+ * Call this only after the streams have been set. */
 void emu_set_cpm_env(i8080 * const cpu);
-/* The starting location of the CP/M Transient Program Area i.e. 
- * the first valid location to load a program written for CP/M. */
-static const emu_addr_t CPM_START_OF_TPA = 0x100;
+
+/* Sets up the default emulator environment.
+ * Creates an interrupt vector table at the top 64 bytes of memory,
+ * and writes a RST 0 sequence that jumps to after the IVT (0x40, DEFAULT_START_PA). */
+void emu_set_default_env(i8080 * const cpu);
 
 // Begin the emulator. Must have properly set up memory and streams first!
-bool emu_runtime(i8080 * const cpu, emu_mem_t * const memory_handle);
+bool emu_runtime(i8080 * const cpu);
 
 void emu_i8080_interrupt(i8080 * const cpu);
 
