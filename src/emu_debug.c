8856c1891c5eb1172e7cff24d438173af15c7b42
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-08-04 22:43:49 -0400

    Reimplement emu_debug functions, implement i8080_debug_next() - Reimplement these functions to use user formats and streams - Implement i8080_debug_next() and set_debug_next_options() -- set_debug_next_options() is required because i8080_debug_next() is expected to have the signature _Bool (*)(i8080 * const) and cannot take any more args

--- src/emu_debug.c
+++ src/emu_debug.c
@@ -2,56 +2,77 @@
  * Implement emu_debug.h
  */
 
+#include <stddef.h>
+#include <string.h>
 #include "emu_debug.h"
 #include "i8080/i8080_opcodes.h"
 
-static const char * DUMP_LOCATION = "dumps/memory_dump.bin";
-static const char * CPU_STATS_DUMP_LOCATION = "dumps/cpu_stats_dump.bin";
-
-void dump_memory(FILE * stream, const emu_word_t * memory, emu_addr_t highest_addr, const char format[]) {
-    
-    FILE * dumpf_ptr = fopen(DUMP_LOCATION, "w");
-    
-    if (dumpf_ptr != NULL) {
-        for (size_t i = 0; i <= (size_t)highest_addr; ++i) {
-            fprintf(dumpf_ptr, WORD_T_FORMAT ",", memory[i]);
-            // End-line every 8 bytes
-            if ((i + 1) % 8 == 0) {
-                fprintf(dumpf_ptr, "\n");
+void dump_memory(FILE * stream, const char format[], int newline_after, emu_addr_t start_addr, emu_addr_t end_addr, i8080 * const cpu) {
+    if (stream != NULL) {
+        fprintf(stream, "**** Memory dump: ****\n");
+        for (emu_addr_t i = start_addr; i <= end_addr; ++i) {
+            fprintf(stream, format, cpu->read_memory(i));
+            // End-line every newline_after words
+            if ((i + 1) % newline_after == 0) {
+                fprintf(stream, "\n");
             }
         }
     }
 }
 
-void dump_cpu_stats(i8080 * const cpu) {
-    
-    FILE * dumpf_ptr = fopen(CPU_STATS_DUMP_LOCATION, "w");
-    
-    if (dumpf_ptr != NULL) {
-        fprintf(dumpf_ptr, "A: " WORD_T_FORMAT "\n", cpu->a);
-        fprintf(dumpf_ptr, "B: " WORD_T_FORMAT "\n", cpu->b);
-        fprintf(dumpf_ptr, "C: " WORD_T_FORMAT "\n", cpu->c);
-        fprintf(dumpf_ptr, "D: " WORD_T_FORMAT "\n", cpu->d);
-        fprintf(dumpf_ptr, "E: " WORD_T_FORMAT "\n", cpu->e);
-        fprintf(dumpf_ptr, "H: " WORD_T_FORMAT "\n", cpu->h);
-        fprintf(dumpf_ptr, "L: " WORD_T_FORMAT "\n", cpu->l);
-        fprintf(dumpf_ptr, "PC: " ADDR_T_FORMAT "\n", cpu->pc);
-        fprintf(dumpf_ptr, "SP: " ADDR_T_FORMAT "\n", cpu->sp);
-        fprintf(dumpf_ptr, "Zero: %d\n", cpu->z);
-        fprintf(dumpf_ptr, "Sign: %d\n", cpu->s);
-        fprintf(dumpf_ptr, "Parity: %d\n", cpu->p);
-        fprintf(dumpf_ptr, "Auxiliary carry: %d\n", cpu->acy);
-        fprintf(dumpf_ptr, "Carry: %d\n", cpu->cy);
-        fprintf(dumpf_ptr, "Interrupt enable: %d\n", cpu->ie);
+void dump_cpu_stats(FILE * stream, i8080 * const cpu) {
+    if (stream != NULL) {
+        fprintf(stream, "**** CPU status: ****\n");
+        fprintf(stream, "A: " WORD_T_FORMAT ", ", cpu->a);
+        fprintf(stream, "B: " WORD_T_FORMAT ", ", cpu->b);
+        fprintf(stream, "C: " WORD_T_FORMAT ",\n", cpu->c);
+        fprintf(stream, "D: " WORD_T_FORMAT ", ", cpu->d);
+        fprintf(stream, "E: " WORD_T_FORMAT ", ", cpu->e);
+        fprintf(stream, "H: " WORD_T_FORMAT ", ", cpu->h);
+        fprintf(stream, "L: " WORD_T_FORMAT ",\n", cpu->l);
+        fprintf(stream, "PC: " ADDR_T_FORMAT ", ", cpu->pc);
+        fprintf(stream, "SP: " ADDR_T_FORMAT "\n", cpu->sp);
+        fprintf(stream, "Zero: %d, ", cpu->z);
+        fprintf(stream, "Sign: %d, ", cpu->s);
+        fprintf(stream, "Parity: %d,\n", cpu->p);
+        fprintf(stream, "Auxiliary carry: %d, ", cpu->acy);
+        fprintf(stream, "Carry: %d, ", cpu->cy);
+        fprintf(stream, "Interrupt enable: %d\n", cpu->ie);
     }
 }
 
-_Bool i8080_debug_next(i8080 * const cpu, FILE * stream, const char mem_dump_format[]) {
+// Debug next options, set to default values
+static FILE * DEBUG_OUT = NULL;
+static const int DUMPF_BUF_SIZE = 128;
+static char MEM_DUMPF_BUF[DUMPF_BUF_SIZE] = WORD_T_FORMAT;
+static int MEM_NEWLINE_AFTER = 16;
+
+/* i8080_debug_next() is expected to have the signature _Bool (*)(i8080 * const),
+ * so the options must be set before calling it. */
+void set_debug_next_options(FILE * stream, const char mem_dump_format[], int mem_dump_newline_after) {
+    // Get size of string along with trailing NULL
+    size_t format_size = sizeof(mem_dump_format);
+    if (format_size > DUMPF_BUF_SIZE) {
+        printf("Error: Debug next format too large.\n");
+    } else {
+        DEBUG_OUT = stream;
+        strncpy(MEM_DUMPF_BUF, mem_dump_format, format_size);
+        MEM_NEWLINE_AFTER = mem_dump_newline_after;
+    }
+}
+
+_Bool i8080_debug_next(i8080 * const cpu) {
+    // Print the instruction
     emu_word_t opcode = i8080_advance_read_word(cpu);
     if (opcode == EMU_EXT_CALL) {
         printf("Emulator external call.\n");
     } else {
         printf("%s\n", DEBUG_DISASSEMBLY_TABLE[opcode]);
     }
-    return i8080_exec(cpu, opcode);
+    // execute the instruction
+    _Bool exec = i8080_exec(cpu, opcode);
+    // Dump stats and memory
+    dump_cpu_stats(DEBUG_OUT, cpu);
+    dump_memory(DEBUG_OUT, MEM_DUMPF_BUF, MEM_NEWLINE_AFTER, cpu);
+    return exec;
 }
--- src/emu_debug.h
+++ src/emu_debug.h
@@ -14,15 +14,18 @@
 #include "emu.h"
 #include <stdio.h>
 
-// Dumps the contents of memory to file
-void dump_memory(const emu_word_t * memory, emu_addr_t highest_addr);
-// Dumps the values of all registers and flags
-void dump_cpu_stats(i8080 * const cpu);
+/* Dumps the contents of memory from start_addr to end_addr to a stream, formatting each word as format.
+ * newline_after is the number of words after which an newline should be inserted each time. */
+void dump_memory(FILE * stream, const char format[], int newline_after, emu_addr_t start_addr, emu_addr_t end_addr, i8080 * const cpu);
+// Writes the values of all flags and registers to the given stream.
+void dump_cpu_stats(FILE * stream, i8080 * const cpu);
 
+// Set the options to be used with the next call to i8080_debug_next().
+void set_debug_next_options(FILE * stream, const char mem_dump_format[], int mem_dump_newline_after);
 /* Performs an i8080_next(), and prints debug information to the given stream:
  * - The instruction executed
  * - State of all registers and flags in the i8080
- * - Dump of the main memory, formatted as given format. */
-_Bool i8080_debug_next(i8080 * const cpu, FILE * stream, const char mem_dump_format[]);
+ * - Dump of the main memory, formatted as given format. A newline is inserted every mem_dump_newline_after words. */
+_Bool i8080_debug_next(i8080 * const cpu);
 
 #endif /* EMU_DEBUG_H */
