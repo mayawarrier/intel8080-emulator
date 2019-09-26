6317fbe9b1961ba40db0275e943d1c7bdcb40933
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-09-26 03:01:32 -0400

    Implement front-end cmdline tool - main.cpp acts as a cmdline processing tool, fetches multiple arguments from command line - run.cpp contains all the logic to execute different cmdline commands, providing examples and documentation on using libi8080emu - Commands supported are: --help --file <name> --env <cpm/default> --run-all-tests - TODO: Fix extraneous "Use --file to specify..." message appearing when only --help is used as an argument

--- main.cpp
+++ main.cpp
@@ -1,89 +1,204 @@
 
 #include "src/emu.h"
-#include "src/emu_consts.h"
 #include "src/i8080/internal/i8080_consts.h"
-#include <cstdio>
 #include <cstdlib>
-#include <atomic>
-#include <thread>
-#include <chrono>
 
-// Emulator main memory, 64KB
-static emu_word_t MEMORY[ADDR_T_MAX + 1];
+#include <iostream>
+#include <fstream>
+#include <functional>
+#include <vector>
+#include <tuple>
+#include <limits>
 
-// Default memory read/write streams
-static emu_word_t rw_from_memory(emu_addr_t addr) { 
-    return MEMORY[addr]; 
+// remove macros coming from C
+#undef max
+#undef min
+
+struct cmd_state {
+    bool is_args_valid;
+    cmd_state() : is_args_valid(false) {}
+};
+
+struct emu_cmd_state : cmd_state {
+    bool is_cpm_env;
+    bool is_runnable;
+    bool is_testing;
+    std::string bin_file;
+    emu_cmd_state() : cmd_state(), 
+        is_cpm_env(false), 
+        is_runnable(false), 
+        is_testing(false),
+        bin_file("") {}
+};
+
+void print_help_msg() {
+    std::cerr << "i8080-emu, an emulator for the INTEL 8080 microprocessor with some CP/M 2.2 BIOS support.\n"
+        << "Supports async interrupts, CPM 2.2 BDOS ops 2 and 9, and a simple command processor at CP/M WBOOT.\n\n"
+        << "Usage: i8080-emu [options]\n"
+        << "Options:\n"
+        << "   -h\t--help\t\t\tPrint this help message.\n"
+        << "   -e\t--env ENV\t\tSet the environment. \"default\" or \"cpm\".\n"
+        << "   -f\t--file FILE\t\tExecute the file as i8080 binary.\n"
+        << "   --run-all-tests\t\tRun all the test files under tests/." << std::endl;
 }
-static void ww_to_memory(emu_addr_t addr, emu_word_t word) { 
-    MEMORY[addr] = word; 
+
+// Prints the help message and returns EXIT failure
+int cmd_print_help_msg_and_exit(cmd_state & cmd_state, std::vector<std::string>::iterator &) {
+    emu_cmd_state & emu_cmd_state = *(struct emu_cmd_state *)(&cmd_state);
+    print_help_msg();
+    // if args were valid, return normally i.e. came here from --help/-h
+    return (int)emu_cmd_state.is_args_valid;
 }
 
-// CPU port out for the CP/M environment.
-static void cpm_env_port_out(emu_addr_t addr, emu_word_t word) {
-    // Address is duplicated, pick lower 8 bits
-    emu_word_t port_addr = (emu_word_t)(addr & WORD_T_MAX);
-    if (port_addr == CPM_CONSOLE_ADDR) {
-        printf(WORD_T_ASCII_FORMAT, word);
+int cmd_set_emu_env(cmd_state & cmd_state, std::vector<std::string>::iterator & opt_args_itr) {
+    int success = 1;
+    emu_cmd_state & emu_cmd_state = *(struct emu_cmd_state *)(&cmd_state);
+    if (*opt_args_itr == "0" || *opt_args_itr == "DEFAULT" || *opt_args_itr == "default") {
+        emu_cmd_state.is_cpm_env = false;
+    } else if (*opt_args_itr == "1" || *opt_args_itr == "CPM" || *opt_args_itr == "cpm") {
+        emu_cmd_state.is_cpm_env = true;
+    } else {
+        std::cout << "--env value invalid. Try --env cpm or --env default." << std::endl;
+        success = 0;
     }
+    return success;
 }
 
-// CPU port in for the CP/M environment.
-static emu_word_t cpm_env_port_in(emu_addr_t addr) {
-    emu_word_t rw = 0x00;
-    // Address is duplicated, pick lower 8 bits
-    emu_word_t port_addr = (emu_word_t)(addr & WORD_T_MAX);
-    if (port_addr == CPM_CONSOLE_ADDR) {
-        scanf(WORD_T_ASCII_FORMAT, &rw);
+int cmd_set_bin_file(cmd_state & cmd_state, std::vector<std::string>::iterator & opt_args_itr) {
+    int success = 0;
+    emu_cmd_state & emu_cmd_state = *(struct emu_cmd_state *)(&cmd_state);
+    std::string & filename = *opt_args_itr;
+    // Check if file exists and if it is at most 64KB in size
+    std::ifstream bin_fstream(filename, std::ios::in | std::ios::binary);
+    if (bin_fstream.is_open()) {
+        // Extract all characters
+        bin_fstream.ignore(std::numeric_limits<std::streamsize>::max());
+        // check file read failure or too large
+        if (!bin_fstream.fail() && bin_fstream.eof()) {
+            std::streamsize length = bin_fstream.gcount();
+            if (length > ADDR_T_MAX + 1) {
+                std::cout << "File too large." << std::endl;
+            } else {
+                // success, file is readable and within size limits
+                emu_cmd_state.bin_file = filename;
+                emu_cmd_state.is_runnable = true;
+                success = 1;
+            }
+        } else {
+            std::cout << "File read error." << std::endl;
+        }
+        bin_fstream.close();
+    } else {
+        std::cout << "File could not be opened." << std::endl;
     }
-    return rw;
+    return success;
 }
 
-static emu_word_t i8080_interrupt_handler(void) {
-    return 0xc7; // RST 0, this exits to WBOOT in the CP/M environment
+int cmd_set_test_run(cmd_state & cmd_state, std::vector<std::string>::iterator &) {
+    emu_cmd_state & emu_cmd_state = *(struct emu_cmd_state *)(&cmd_state);
+    emu_cmd_state.is_runnable = true;
+    emu_cmd_state.is_testing = true;
+    return 1;
 }
+
+using cmd_callback_fn = std::function<int(cmd_state & cmd_state, std::vector<std::string>::iterator & cmdline_args)>;
+using cmd_handle = std::tuple<std::string, std::string, bool, cmd_callback_fn>;
+
+// {<fullname>, <alias>, <has_args>, <callback>}
+const std::vector<cmd_handle> CMDLINE_COMMANDS {
+    cmd_handle("--help", "-h", false, cmd_callback_fn(cmd_print_help_msg_and_exit)),
+    cmd_handle("--env", "-e", true, cmd_callback_fn(cmd_set_emu_env)),
+    cmd_handle("--file", "-f", true, cmd_callback_fn(cmd_set_bin_file)),
+    cmd_handle("--run-all-tests", "", false, cmd_callback_fn(cmd_set_test_run))
+};
+
+int process_cmdline(int argc, char ** argv, cmd_state & cmd_state) {
+
+    std::vector<std::string> CMDLINE_ARGS;
+    CMDLINE_ARGS.reserve(3);
+    // if the last command exec was successful
+    int exec_success = 0;
+
+    if (argc < 2) {
+        // not enough args, print help message and exit
+        cmd_state.is_args_valid = false;
+        return cmd_print_help_msg_and_exit(cmd_state, CMDLINE_ARGS.begin());
+    } else {
+        cmd_state.is_args_valid = true;
+
+        // copy the cmd line args in argv
+        for (int i = 1; i < argc; ++i) {
+            CMDLINE_ARGS.push_back(std::string(argv[i]));
+        }
+
+        // Function that is called when command is identified
+        cmd_callback_fn cmd_callback;
+        bool cmd_has_args;
+
+        auto args_itr = CMDLINE_ARGS.begin();
+        while (args_itr != CMDLINE_ARGS.end()) {
+            cmd_callback = nullptr;
+            cmd_has_args = false;
+
+            std::string & arg = *args_itr;
+            // Check against all commands
+            for (std::size_t j = 0; j < CMDLINE_COMMANDS.size(); ++j) {
+                const cmd_handle & cmd_handle = CMDLINE_COMMANDS[j];
+                // check if matches full name or alias
+                const std::string & cmd_name = std::get<0>(cmd_handle);
+                const std::string & cmd_alias = std::get<1>(cmd_handle);
+                if (arg == cmd_name || (!cmd_alias.empty() && arg == cmd_alias)) {
+                    cmd_has_args = std::get<2>(cmd_handle);
+                    cmd_callback = std::get<3>(cmd_handle);
+                }
+            }
+
+            if (cmd_callback) {
+                // check if cmd args are past end of cmdline args
+                args_itr++;
+                if (cmd_has_args && args_itr == CMDLINE_ARGS.end()) {
+                    std::cout << arg.c_str() << ": Missing arguments. See \"--help\" for usage." << std::endl;
+                    exec_success = 0;
+                } else {
+                    exec_success = cmd_callback(cmd_state, args_itr);
+                    // if the cmd had args, move past its last arg
+                    if (cmd_has_args) args_itr++;
+                }
+            } else {
+                std::cout << arg.c_str() << " is not a command. See \"--help\" for usage." << std::endl;
+                exec_success = 0;
+            }
+
+            // if any command execution fails, quit
+            if (!exec_success) break;
+        }
+    }
+
+    return exec_success;
+}
+
+// See run.cpp. These functions show example usage of lib-i8080emu.
+int run_all_tests();
+int run_i8080_bin(const std::string & bin_file, bool is_cpm_env);
 
 int main(int argc, char ** argv) {
+    int exit_success = 0;
+    //if (EMU_ENV == (enum emu_env)CPM) printf("\nCP/M Warm Boot. Type HELP for a list of commands.");
+    emu_cmd_state EMU_CMD_STATE;
+    int cmd_success = process_cmdline(argc, argv, EMU_CMD_STATE);
 
-    const char * test_file_location = "../tests/test_interrupts.COM";
-
-    if (test_file_location == NULL) {
-        printf("Error: No file provided.\n");
-        return EXIT_FAILURE;
+    if (cmd_success && EMU_CMD_STATE.is_runnable) {
+        if (EMU_CMD_STATE.is_testing) {
+            run_all_tests();
+        } else {
+            run_i8080_bin(EMU_CMD_STATE.bin_file, EMU_CMD_STATE.is_cpm_env);
+        }
+        exit_success = EXIT_SUCCESS;
     }
-
-    // Make an emulated i8080
-    i8080 cpu;
-    std::atomic<emu_exit_code_t> ON_EMU_EXIT;
-
-    // Initialize i8080 and setup default memory + IO streams, interrupt handler
-    emu_init_i8080(&cpu);
-    cpu.read_memory = rw_from_memory;
-    cpu.write_memory = ww_to_memory;
-    cpu.port_in = cpm_env_port_in;
-    cpu.port_out = cpm_env_port_out;
-    cpu.interrupt_acknowledge = i8080_interrupt_handler;
-
-    // Read file into memory after CP/M reserved space
-    if (!memory_check_errors(&cpu, 0, ADDR_T_MAX, 0) || memory_load(test_file_location, MEMORY, CPM_START_OF_TPA) == 0)
-        return EXIT_FAILURE;
-
-    // Set up BDOS and WBOOT emulation for CP/M
-    emu_set_cpm_env(&cpu);
-
-    // Begin the emulator and an interrupt generator
-    std::thread emu_runtime_thread([&] {
-        ON_EMU_EXIT = emu_runtime(&cpu, NULL);
-    });
-    std::thread intgen_thread([&] {
-        // sleep for 10s then send an interrupt
-        std::this_thread::sleep_for(std::chrono::seconds(10));
-        i8080_interrupt(&cpu);
-    });
-
-    emu_runtime_thread.join();
-    intgen_thread.join();
-
-    if (ON_EMU_EXIT == EMU_EXIT_CODE::EMU_EXIT_SUCCESS) return EXIT_SUCCESS;
-    else return EXIT_FAILURE;
+    // not runnable, no file provided
+    if (!EMU_CMD_STATE.is_runnable && cmd_success) { // TODO: check that --help is not called on its own before printing msg
+        std::cout << "Use --file to specify source of i8080 binary." << std::endl;
+        exit_success = EXIT_FAILURE;
+    }
+    return exit_success;
 }
---
+++ run.cpp
@@ -0,0 +1,186 @@
+#include "src/emu.h"
+#include "src/emu_consts.h"
+#include "src/i8080/internal/i8080_consts.h"
+
+#include <cstdio> // for easy formatted printing
+#include <cstring> // memset
+#include <iostream>
+#include <atomic>
+#include <thread>
+#include <chrono>
+
+// 64KB of emulated memory
+static emu_word_t MEMORY[ADDR_T_MAX + 1];
+
+// Default memory read/write streams
+static emu_word_t rw_from_memory(emu_addr_t addr) {
+    return MEMORY[addr];
+}
+static void ww_to_memory(emu_addr_t addr, emu_word_t word) {
+    MEMORY[addr] = word;
+}
+
+// CPU port out for the CP/M environment.
+static void cpm_env_port_out(emu_addr_t addr, emu_word_t word) {
+    // Address is duplicated, pick lower 8 bits
+    emu_word_t port_addr = (emu_word_t)(addr & WORD_T_MAX);
+    if (port_addr == CPM_CONSOLE_ADDR) {
+        printf(WORD_T_ASCII_FORMAT, word);
+    }
+}
+
+// CPU port in for the CP/M environment.
+static emu_word_t cpm_env_port_in(emu_addr_t addr) {
+    emu_word_t rw = 0x00;
+    // Address is duplicated, pick lower 8 bits
+    emu_word_t port_addr = (emu_word_t)(addr & WORD_T_MAX);
+    if (port_addr == CPM_CONSOLE_ADDR) {
+        scanf(WORD_T_ASCII_FORMAT, &rw);
+    }
+    return rw;
+}
+
+// Minimum required calls to set up i8080 emulator.
+i8080 init_i8080_emu_default() {
+    i8080 cpu;
+    /*
+       Clears interrupt bit, pending interrupts, HLT,
+       memory+I/O streams, cycles_taken, and resets PC.
+       Registers, stack pointer and flags are not initialized/cleared,
+       the program is expected to initialize them by itself.
+    */
+    emu_init_i8080(&cpu);
+    // Assign memory stream fns
+    cpu.read_memory = rw_from_memory;
+    cpu.write_memory = ww_to_memory;
+    /*
+       I/O stream fns can also be assigned:
+       cpu.port_in = <i8080.h/emu_read_word_fp>;
+       cpu.port_out = <i8080.h/emu_write_word_fp>;
+       Assigning these are not necessary but I/O
+       requests will cause the emulator to quit.
+
+       Hardware interrupts can also be enabled,
+       if the build environment supports mutexes (see i8080_sync.h):
+       cpu.interrupt_acknowledge = <emu_word_t(*)(void)>;
+       interrupt_acknowledge() should return the word to be executed on interrupt.
+       If not assigned, calls to i8080_interrupt() will be ignored.
+    */
+    /*
+       Set default environment, with no CP/M BIOS support.
+       This places an IVT at the first 64 bytes, and a boot sequence
+       at RST 0 which jumps to 0x40.
+
+       CP/M 2.2 environment can be set instead with emu_set_cpm_env(&cpu, <enable_cmd_proc>).
+       This provides a simple command processor at CP/M WBOOT, and adds
+       support for BDOS operations 2 and 9. To disable the command
+       processor, set enable_cmd_proc = 0.
+       - The first 0x100 bytes are reserved for the BIOS, and locations
+         from 0xe400 onwards are reserved for CP/M (assuming a 64K machine).
+    */
+    emu_set_default_env(&cpu);
+    return cpu;
+}
+
+// See init_i8080_emu_default for documentation
+i8080 init_i8080_emu_cpm() {
+    i8080 cpu;
+    emu_init_i8080(&cpu);
+    cpu.read_memory = rw_from_memory;
+    cpu.write_memory = ww_to_memory;
+    cpu.port_in = cpm_env_port_in;
+    cpu.port_out = cpm_env_port_out;
+    // Initialize in CPM mode, disable cmd processor since this is used for tests
+    emu_set_cpm_env(&cpu, 0);
+    return cpu;
+}
+
+static int run_generic_test(i8080 * const cpu, const std::string & file_location, emu_addr_t program_load_loc);
+static int run_interrupted_test(i8080 * const cpu, const std::string & file_location, emu_addr_t program_load_loc, int ms_to_interrupt);
+
+int run_all_tests() {
+    // test locations
+    const std::string interrupts_test_fileloc = "tests/test_interrupts.COM";
+    const std::string tst8080_test_fileloc = "tests/TST8080.COM";
+    const std::string cputest_test_fileloc = "tests/CPUTEST.COM";
+
+    // All tests are run in CP/M env
+    i8080 cpu = init_i8080_emu_cpm();
+
+    // ---------------- running tests ------------------------
+    const int num_tests = 3;
+    int num_tests_passed = 0;
+    // Clears the entire TPA (till 0xe400). EEPROMS were commonly blanked with 0xff.
+    const auto clear_cpm_tpa = []() {std::memset(&MEMORY[CPM_START_OF_TPA], 0xff, 0xe300);};
+
+    clear_cpm_tpa();
+    std::cout << "\n-------------------------- Test 1: TST8080.COM --------------------------\n";
+    std::cout << "\t\t8080/8085 CPU Diagnostic, Kelly Smith, 1980";
+    std::cout << "\n-------------------------------------------------------------------------\n";
+    num_tests_passed += run_generic_test(&cpu, tst8080_test_fileloc, CPM_START_OF_TPA);
+    std::cout << "\n-------------------------------------------------------------------------\n";
+
+    clear_cpm_tpa();
+    i8080_reset(&cpu);
+    std::cout << "\n-------------------------- Test 2: CPUTEST.COM --------------------------\n";
+    std::cout << "\tSupersoft Associates CPU test, Diagnostics II suite, 1981";
+    std::cout << "\n-------------------------------------------------------------------------\n";
+    num_tests_passed += run_generic_test(&cpu, cputest_test_fileloc, CPM_START_OF_TPA);
+    std::cout << "\n-------------------------------------------------------------------------\n";
+
+    clear_cpm_tpa();
+    i8080_reset(&cpu);
+    std::cout << "\n---------------------- Test 3: test_interrupts.COM ----------------------\n";
+    std::cout << "\t\tPrints '+' in a loop for 0.2s, then returns to \n\t\tWBOOT upon receiving interrupt.";
+    std::cout << "\n-------------------------------------------------------------------------\n";
+    num_tests_passed += run_interrupted_test(&cpu, interrupts_test_fileloc, CPM_START_OF_TPA, 200);
+    std::cout << "\n-------------------------------------------------------------------------\n";
+
+    std::cout << "\n(" << num_tests_passed << "/" << num_tests << ") tests passed." << std::endl;
+    i8080_destroy(&cpu);
+
+    return (num_tests_passed == 3) ? 1 : 0;
+}
+
+int run_i8080_bin(const std::string & bin_file_loc, bool is_cpm_env) {
+    std::cout << std::endl;
+    i8080 cpu = is_cpm_env ? init_i8080_emu_cpm() : init_i8080_emu_default();
+    emu_addr_t program_start_loc = is_cpm_env ? CPM_START_OF_TPA : DEFAULT_START_PA;
+    int success = run_generic_test(&cpu, bin_file_loc, program_start_loc);
+    i8080_destroy(&cpu);
+    std::cout << std::endl;
+    return success;
+}
+
+static int run_generic_test(i8080 * const cpu, const std::string & file_location, emu_addr_t program_load_loc) {
+    if (memory_load(file_location.c_str(), MEMORY, program_load_loc) == 0) return 0;
+    if (emu_runtime(cpu, NULL) == EMU_EXIT_CODE::EMU_EXIT_SUCCESS) return 1;
+    else return 0;
+}
+
+static emu_word_t i8080_interrupt_handler(void) {
+    return 0xc7; // RST 0, this exits to WBOOT in the CP/M environment
+}
+
+static int run_interrupted_test(i8080 * const cpu, const std::string & file_location, emu_addr_t program_load_loc, int ms_to_interrupt) {
+    // Enable hardware interrupts
+    cpu->interrupt_acknowledge = i8080_interrupt_handler;
+    if (memory_load(file_location.c_str(), MEMORY, program_load_loc) == 0) return 0;
+
+    std::atomic<emu_exit_code_t> on_emu_exit;
+
+    // Begin the emulator and send an interrupt after a period of time
+    std::thread emu_runtime_thread([&] {
+        on_emu_exit = emu_runtime(cpu, NULL);
+    });
+    std::thread intgen_thread([&] {
+        std::this_thread::sleep_for(std::chrono::milliseconds(ms_to_interrupt));
+        i8080_interrupt(cpu);
+    });
+
+    emu_runtime_thread.join();
+    intgen_thread.join();
+
+    if (on_emu_exit == EMU_EXIT_CODE::EMU_EXIT_SUCCESS) return 1;
+    else return 0;
+}
