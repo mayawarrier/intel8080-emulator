469cdce488ba1e730764fcf8f197b776c2302cb3
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-11-05 16:18:03 -0500

    Complete: Rename types, provide portable minimum types for 8 bit and 16 bit fields

--- libi8080emu/libi8080/include/i8080_types.h
+++ libi8080emu/libi8080/include/i8080_types.h
@@ -22,7 +22,7 @@
     #endif
 #endif
 
-/* Determine i8080_mutex_t for different environments. */
+/* Try to find a mutex/synchronization primitive */
 #ifdef I8080_WINDOWS_MIN_VER
     /* Critical sections have better performance than standard
      * Windows mutexes and appear to be compatible with std::thread. */
@@ -36,55 +36,47 @@
     /* Simulate a mutex using acquire - release semantics on a char. */
     I8080_CDECL typedef char i8080_mutex_t;
 #else
-    /* A volatile char ~should~ provide the best chance at proper sync, 
+    /* A volatile char ~should~ provide the best chance at proper sync,
      * if nothing else is available. */
     I8080_CDECL typedef volatile char i8080_mutex_t;
 #endif
 
-/* */
-#if (defined(__STDC_VERSION__) || defined(__cplusplus) || (defined(I8080_WINDOWS) && _MSC_VER > 0000)) /* TODO: Placeholder, add real version check for MSVC! */
-    /* Use stdint fixed types if available */
+/* Use stdint fixed width types if available */
+#if (defined(__STDC_VERSION__) || defined(__cplusplus) || defined(I8080_WINDOWS_MIN_VER)) /* TODO: Use libgit2's stdint.h for bad msvc configs */
     #include <stdint.h>
 #endif
 
-#if defined(UINT8_MAX)
-    typedef uint8_t __i8080_uint_least8_t__;
-#elif defined(UINT16_MAX)
-    typedef uint16_t __i8080_uint_least8_t__;
-#else 
-    /* If CHAR_BIT is 8, we can use that as a byte */
-    #include <limits.h>
-    #if (CHAR_BIT == 8)
-        typedef unsigned char __i8080_uint_least8_t__;
-    #else
-        #error "CHAR_BIT is not 8! Provide the type __i8080_uint_least8_t__"
-    #endif
+/* Determine safe base types __i8080_size_t__ and __i8080_dbl_size_t__ 
+ * These are guaranteed to hold at least 8 and 16 bits respectively */
+#if defined(UINT8_MAX) && defined(UINT16_MAX)
+    typedef uint8_t __i8080_size_t__;
+    typedef uint16_t __i8080_dbl_size_t__;
+#else
+    /* C89 guarantees uchar and uint to be 8 bits and 16 bits at least */
+    typedef unsigned char __i8080_size_t__;
+    typedef unsigned int __i8080_dbl_size_t__;
 #endif
 
-/* Safe base types */
-typedef __i8080_uint_least8_t__ __i8080_size_t__;
-typedef struct  __i8080_dbl_size__ {
-    __i8080_uint_least8_t__ lo : 8;
-    __i8080_uint_least8_t__ hi : 8;
-} __i8080_dbl_size_t__;
-
-/* i8080-emu base types follow.
- * These can be modified for the desired host/virtual machine.
+/* i8080-emu base types
  *
- * emu_word_t: An unsigned type with a bit width at least equal to the virtual machine's word size.
- * emu_addr_t: An unsigned type with a bit width at least equal to the virtual machine's address size.
- * emu_buf_t: An unsigned type that is double the size of emu_word_t and at least 1 bit larger than emu_addr_t.
- * emu_large_t: An unsigned type to count processor cycles.
+ * i8080_word_t: unsigned, size at least 8.
+ * i8080_addr_t: unsigned, size at least 16.
+ * i8080_dbl_word_t: unsigned, at least double the size of i8080_word_t.
+ * i8080_uintmax_t: unsigned, counts processor cycles.
  */
+I8080_CDECL typedef __i8080_size_t__ i8080_word_t;
+I8080_CDECL typedef __i8080_dbl_size_t__ i8080_addr_t;
+I8080_CDECL typedef __i8080_dbl_size_t__ i8080_dbl_word_t;
+#ifdef UINTMAX_MAX
+    I8080_CDECL typedef uintmax_t i8080_uintmax_t;
+#else
+    /* Largest portable type in C89 */
+    I8080_CDECL typedef unsigned long int i8080_uintmax_t;
+#endif
 
-/* Construct base types from safe types above */
-I8080_CDECL typedef __i8080_size_t__ emu_word_t;
-I8080_CDECL typedef __i8080_dbl_size_t__ emu_addr_t;
-I8080_CDECL typedef __i8080_dbl_size_t__ emu_buf_t;
-I8080_CDECL typedef unsigned long long int emu_large_t;
-
-I8080_CDECL typedef emu_word_t(*emu_read_word_fp)(emu_addr_t);
-I8080_CDECL typedef void(*emu_write_word_fp)(emu_addr_t, emu_word_t);
+/* Read/write streams for the i8080 */
+I8080_CDECL typedef i8080_word_t(*i8080_read_word_fp)(i8080_addr_t);
+I8080_CDECL typedef void(*i8080_write_word_fp)(i8080_addr_t, i8080_word_t);
 
 #include "i8080_predef_undef.h"
 
