8f7901b313db0a27d1842fc954f6da74a0febdf5
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-11-04 21:24:51 -0500

    Redo types, get a byte type from stdint only, if available - If not available, it uses a 16 bit type as a bitfield substitute for a byte - Byte type is named __i8080_uint_least8_t__. This is typedefd to __i8080_size_t__, which has at least enough space to hold an i8080 word - Similarly __i8080_dbl_size_t__ has at least enough space to store two i8080 words

--- libi8080emu/libi8080/include/i8080_types.h
+++ libi8080emu/libi8080/include/i8080_types.h
@@ -1,5 +1,5 @@
 /*
- * Provides definitions for all i8080 types and function pointers,
+ * Provides definitions for all i8080 base types and function pointers,
  * and suppresses warnings & errors from MSVC. If including this in an MSVC project,
  * it should be included before any Windows header or before including <stdio.h>.
  */
@@ -41,77 +41,47 @@
     I8080_CDECL typedef volatile char i8080_mutex_t;
 #endif
 
-/* i8080-emu base types follow.
- * These can be modified for the desired host/virtual machine.
- *
- * emu_word_t: An unsigned type with a bit width equal to the virtual machine's word size.
- * emu_addr_t: An unsigned type with a bit width equal to the virtual machine's address size.
- * emu_buf_t: An unsigned type that is double the size of emu_word_t and at least 1 bit larger than emu_addr_t.
- * emu_large_t: An unsigned large type. Used to count processor cycles taken. If this is not large enough,
- *             the emulator will still run properly, but cycles_taken might overflow.
- */
-
+/* */
 #if (defined(__STDC_VERSION__) || defined(__cplusplus) || (defined(I8080_WINDOWS) && _MSC_VER > 0000)) /* TODO: Placeholder, add real version check for MSVC! */
-    /* If stdint is available, we may get some fixed width types from there */
+    /* Use stdint fixed types if available */
     #include <stdint.h>
-#else
-    /* If CHAR_BIT is 8, we can construct types from a uchar */
+#endif
+
+#if defined(UINT8_MAX)
+    typedef uint8_t __i8080_uint_least8_t__;
+#elif defined(UINT16_MAX)
+    typedef uint16_t __i8080_uint_least8_t__;
+#else 
+    /* If CHAR_BIT is 8, we can use that as a byte */
     #include <limits.h>
     #if (CHAR_BIT == 8)
-        typedef unsigned char __i8080_byte_t__;
+        typedef unsigned char __i8080_uint_least8_t__;
     #else
-        #error "CHAR_BIT is not 8! Provide your own byte-sized type."
+        #error "CHAR_BIT is not 8! Provide the type __i8080_uint_least8_t__"
     #endif
 #endif
 
-#ifdef UINT8_MAX
-    typedef uint8_t __i8080_uint8_t__;
-#else
-    typedef __i8080_byte_t__ __i8080_uint8_t__;
-#endif
+/* Safe base types */
+typedef __i8080_uint_least8_t__ __i8080_size_t__;
+typedef struct  __i8080_dbl_size__ {
+    __i8080_uint_least8_t__ lo : 8;
+    __i8080_uint_least8_t__ hi : 8;
+} __i8080_dbl_size_t__;
 
-#ifdef UINT16_MAX
-    typedef uint16_t __i8080_uint16_t__;
-#else
-    typedef struct {
-        __i8080_byte_t__ b0;
-        __i8080_byte_t__ b1;
-    } __i8080_uint16_t__;
-#endif
-
-#ifdef UINT32_MAX
-    typedef uint32_t __i8080_uint32_t__;
-#else
-    typedef struct {
-        __i8080_byte_t__ b0;
-        __i8080_byte_t__ b1;
-        __i8080_byte_t__ b2;
-        __i8080_byte_t__ b3;
-    } __i8080_uint32_t__;
-#endif
-
-#ifdef UINT64_MAX
-    typedef uint64_t __i8080_uint64_t__;
-#else
-    typedef struct {
-        __i8080_byte_t__ b0;
-        __i8080_byte_t__ b1;
-        __i8080_byte_t__ b2;
-        __i8080_byte_t__ b3;
-        __i8080_byte_t__ b4;
-        __i8080_byte_t__ b5;
-        __i8080_byte_t__ b6;
-        __i8080_byte_t__ b7;
-    } __i8080_uint64_t__;
-#endif
+/* i8080-emu base types follow.
+ * These can be modified for the desired host/virtual machine.
+ *
+ * emu_word_t: An unsigned type with a bit width at least equal to the virtual machine's word size.
+ * emu_addr_t: An unsigned type with a bit width at least equal to the virtual machine's address size.
+ * emu_buf_t: An unsigned type that is double the size of emu_word_t and at least 1 bit larger than emu_addr_t.
+ * emu_large_t: An unsigned type to count processor cycles.
+ */
 
 /* Construct base types from safe types above */
-I8080_CDECL typedef __i8080_uint8_t__ emu_word_t;
-I8080_CDECL typedef __i8080_uint16_t__ emu_addr_t;
-I8080_CDECL typedef __i8080_uint32_t__ emu_buf_t;
-I8080_CDECL typedef __i8080_uint64_t__ emu_large_t;
-
-/* */
+I8080_CDECL typedef __i8080_size_t__ emu_word_t;
+I8080_CDECL typedef __i8080_dbl_size_t__ emu_addr_t;
+I8080_CDECL typedef __i8080_dbl_size_t__ emu_buf_t;
+I8080_CDECL typedef unsigned long long int emu_large_t;
 
 I8080_CDECL typedef emu_word_t(*emu_read_word_fp)(emu_addr_t);
 I8080_CDECL typedef void(*emu_write_word_fp)(emu_addr_t, emu_word_t);
