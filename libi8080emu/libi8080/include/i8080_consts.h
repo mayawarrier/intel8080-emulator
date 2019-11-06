5b28684a7f255870854993bfdd5d7cdeccb96eb0
Dhruv Warrier <dhruv.warrier89@gmail.com>
2019-11-06 13:51:08 -0500

    WIP: Rewrite some functions as macros for better inline-ability with c89 compilers - Some functions are duplicated, better as generic macros - Add WORD_SIZE, all references to HALF_WORD_SIZE should be updated to WORD_SIZE / 2 - WIP commit, does not necessarily build or work correctly

--- libi8080emu/libi8080/include/i8080_consts.h
+++ libi8080emu/libi8080/include/i8080_consts.h
@@ -6,7 +6,6 @@
 #ifndef I8080_CONSTS_H
 #define I8080_CONSTS_H
 
-#include <stdint.h>
 #include <inttypes.h>
 
 /* HALF_WORD_SIZE: half the bit width of emu_word_t
@@ -18,9 +17,11 @@
  * WORD_T_PRT_FORMAT : format specifier to print word as ASCII */
 
 #define HALF_WORD_SIZE (4)
+#define WORD_SIZE (8)
+#define ADDR_SIZE (16)
 #define HALF_ADDR_SIZE (8)
-#define WORD_T_MAX UINT8_MAX
-#define ADDR_T_MAX UINT16_MAX
+#define WORD_T_MAX (255)
+#define ADDR_T_MAX (65535)
 
 #define WORD_T_SCN_FORMAT "0x%" SCNx8
 #define ADDR_T_SCN_FORMAT "0x%" SCNx16
